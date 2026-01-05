5d95505ea21a64273e05aa53a6a3e208df7d85eb
realtimepeople <klaus.schaefer@h-da.de>
2026-01-05 12:33:31 +0100

    Added copyright information.

--- CAN-L4_3100/Magnetometer/RM3100.cpp
+++ CAN-L4_3100/Magnetometer/RM3100.cpp
@@ -1,3 +1,27 @@
+/**
+ * @file    RM3100.cpp
+ * @brief   Magnetometer data acquisition module
+ * @author	Dr. Klaus Schaefer
+ * @copyright 	Copyright 2026 Dr. Klaus Schaefer. All rights reserved.
+ * @license 	This project is released under the GNU Public License GPL-3.0
+
+    <Larus Flight Sensor Firmware>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ **************************************************************************/
+
 #include "FreeRTOS_wrapper.h"
 #include "RM3100.hpp"
 #include "stm32l4xx_hal.h"
